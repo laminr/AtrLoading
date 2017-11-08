31598752b99895d602892a967c9dc03c9b156799
Thibault de Lambilly <tdelambilly@eventual.biz>
2017-11-08 21:52:43 +0100

    putting rdo

--- app/src/main/java/biz/eventually/loading/MainActivity.kt
+++ app/src/main/java/biz/eventually/loading/MainActivity.kt
@@ -7,15 +7,28 @@ import biz.eventually.atr.enum.MotionDelay
 import biz.eventually.atr.RunUpBckgRichPath
 import biz.eventually.atr.RunUpLoader
 import android.os.CountDownTimer
+import android.widget.RadioButton
+import android.widget.RadioGroup
 
 class MainActivity : AppCompatActivity() {
 
     private val speeds = listOf(MotionDelay.BY_THE_WIND, MotionDelay.SLOW, MotionDelay.NORMAL, MotionDelay.QUICK)
-    private var index = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val atr = findViewById<RunUpBckgRichPath>(R.id.runup)
+        val rdo = findViewById<RadioGroup>(R.id.rdoGroup)
+        rdo.setOnCheckedChangeListener { group, checkedId ->
+            val theOne = group.findViewById<RadioButton>(checkedId)
+            val which =  theOne.tag.toString().toInt()
+            if (which == 0) {
+                atr.stop()
+            }else {
+                atr.start(speeds[which-1])
+            }
+        }
     }
 
     fun onClickDialog(view: View) {
@@ -34,10 +47,10 @@ class MainActivity : AppCompatActivity() {
         }.start()
     }
 
-    fun onClickSvg(view: View) {
-        val atr = findViewById<RunUpBckgRichPath>(R.id.runup)
-        atr.start(speeds[index])
-        if (++index >= speeds.size) index = 0
-
-    }
+//    fun onClickSvg(view: View) {
+//        val atr = findViewById<RunUpBckgRichPath>(R.id.runup)
+//        atr.start(speeds[index])
+//        if (++index >= speeds.size) index = 0
+//
+//    }
 }
